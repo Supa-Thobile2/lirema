a66f29a0ca484ea18bbb32ab486838f6e58e6c7f
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-05 14:57:18 +0200

    first commit

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -7,7 +7,7 @@ function ServicesCard(props) {
       <img src={img} alt='' className='object-fit w-full h-full'/>
         <div className='space-y-2 p-2 md:pace-y-4 md:p-8'>
             <h6 className='text-sm font-bold font-400'>{props.heading}</h6>
-            <h1 className='text-lg font-bold font-400'>{props.subtext}</h1>
+            <h1 className='text-sm font-bold font-400'>{props.subtext}</h1>
             <p className='text-md font-medium font-200'>{props.description}</p>
             <div>
                 <button className='p-2 border text-md font-bold'>{props.btnText}</button>
