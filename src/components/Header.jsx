be17c935120511d8a90ac4b04fa707f775390a90
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-05 14:01:42 +0200

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
   return (
     
     <div className='
-     border-b-1 sticky w-full inset-0 '>
+     border-b-1 sticky w-full inset-0 p-4'>
         <div className=' p-4 flex items-center justify-between w-[100%] md:hidden'>
             <div  >
                 <p className='text-md font-bold font-400'>Li Rema engineering & Costruction</p>
