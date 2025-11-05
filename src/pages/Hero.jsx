a1173841aa56a5dfcf0af17ca196f77f38385e48
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-05 14:16:42 +0200

    first commit

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
   }
   return (
 
-    <div className=''>
+    <div className='p-2'>
     <div className='w-full mx-auto md:w-3/4  p-8 md:p-24 md:space-y-16 lg:p-18 lg:space-y-18 text-center  flex items-center justify-center flex-col  mx-auto'>
         
         <h1 className='text-md
