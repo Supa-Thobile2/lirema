22bacbeb5f47013c75383fdfda5dfbe20f4240ca
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-25 00:06:16 +0200

    updated footer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button(props) {
   return (
     <div className='w-full'>
-         <button className='px-6 py-2 border rounded-xl w-full mx-auto text-[.8rem] font-medium font-400'>{props.text}</button>
+         <button className='px-6 py-2 border border-[lightgray] rounded-xl w-full mx-auto text-[.8rem] font-medium font-400'>{props.text}</button>
     </div>
   )
 }
