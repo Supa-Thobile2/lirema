f2582e0adc3470ab10959bcb3fcfe96cc3488322
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-24 23:17:34 +0200

    updated subscribe section

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -6,8 +6,8 @@ function Quote() {
   return (
     <div className='block p-4 space-y-8 md:flex items-center justify-center md:p-18'>
         <div className='space-y-12 p-4 md:p-8'>
-            <h1 className='text-4xl font-bold font-600'>Ready to build your future</h1>
-            <p className='text-sm font-medium font-200'>Connect with our expert engineering team to transform your complex technical challenges into innovative solutions</p>
+            <h1 className='text-[1.5rem] font-bold font-600'>Ready to build your future</h1>
+            <p className='text-[.6rem] font-medium font-200'>Connect with our expert engineering team to transform your complex technical challenges into innovative solutions</p>
             <div className='flex items-center gap-6 w-full'>
                <Button text='Quote'/>
                <Button text='Consult'/>
