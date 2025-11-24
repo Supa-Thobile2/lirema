69eee9c9bc0bd826f384876a5265d15140c6eb00
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-24 21:06:56 +0200

    updated about section

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,7 +8,7 @@ import { ServicesCard } from '../components'
 
 function Services() {
   return (
-    <div className='   p-2 space-y-4 block w-full md:flex items-center justify-center  flex-col md:p-16 md:space-y-8 xl:p-16 ' id='services'>
+    <div className='   p-4 space-y-4 block w-full md:flex items-center justify-center  flex-col md:p-16 md:space-y-8 xl:p-16 ' id='services'>
         <p className='text-[.7rem] font-semibold font-300 '>Services</p>
         <div className='space-y-2'>
         <h1 className='text-[1.4rem] font-bold font-700'>Comprehensive technical solutions</h1>
@@ -16,7 +16,7 @@ function Services() {
        
         <p className='text-[.67rem] font-medium font-500'>Integrated engineering services for complex industrial and residential projects</p>
         </div>
-       <div className='block  shadow-xl space-y-2  md:flex items-center justify-center gap-4 md:gap-8'>
+       <div className='block  p-4 shadow-xl space-y-2  md:flex items-center justify-center gap-4 md:gap-8'>
         {servicesData.map((item, index)=>{
             return <ServicesCard
             heading ={item.heading}
