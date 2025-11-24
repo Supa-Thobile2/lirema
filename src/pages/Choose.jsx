43b40b228f62196dc744fc2e4167b2e26081b6ae
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-24 21:56:47 +0200

    updated choose section

--- src/pages/Choose.jsx
+++ src/pages/Choose.jsx
@@ -12,8 +12,8 @@ function Choose() {
        </div>
        
         <div className='block space-y-2 md:flex gap-4 '>
-            <div className='flex items-center justify-between flex-col space-y-4'>
-                <div className='border space-y-2 md:p-8'>
+            <div className='flex items-center justify-between flex-col space-y-2'>
+                <div className='border space-y-2 p-4'>
                     <div className='space-y-2'>
                     <h1 className='text-[1.5rem] font-bold font-700'>Technical expertise</h1>
                     <p className='text-[.6rem] font-medium font-200'>Highly skilled engineers with deep technical knowledge</p>
@@ -22,7 +22,7 @@ function Choose() {
                         <Button text='Explore Capabilities'/>
                     </div>
                 </div>
-                <div className='border  space-y-2 p-4 md:p-8'>
+                <div className='border  space-y-2 p-4 '>
                     <h1 className='text-[1.5rem] font-bold font-700'>Technical expertise</h1>
                     <p className='text-[.6rem] font-medium font-200'>Highly skilled engineers with deep technical knowledge</p>
                     <div>
