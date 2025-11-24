aa160e2af6b0e73fcb124ed59f7f35224bdb4f0f
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-24 21:02:01 +0200

    updated about section

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -10,12 +10,13 @@ function Services() {
   return (
     <div className='   p-2 space-y-4 block w-full md:flex items-center justify-center  flex-col md:p-16 md:space-y-8 xl:p-16 ' id='services'>
         <p className='text-[.7rem] font-semibold font-300 '>Services</p>
+        <div className='space-y-2'>
         <h1 className='text-[1.4rem] font-bold font-700'>Comprehensive technical solutions</h1>
        
        
         <p className='text-[.67rem] font-medium font-500'>Integrated engineering services for complex industrial and residential projects</p>
-        
-       <div className='block  shadow-xl space-y-4  md:flex items-center justify-center gap-4 md:gap-8'>
+        </div>
+       <div className='block  shadow-xl space-y-2  md:flex items-center justify-center gap-4 md:gap-8'>
         {servicesData.map((item, index)=>{
             return <ServicesCard
             heading ={item.heading}
