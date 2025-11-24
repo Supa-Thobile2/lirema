1196fa3952e4654bdb783fe47b9cdb42900fe4b3
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-25 01:14:28 +0200

    updated footer section

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -16,47 +16,7 @@ function ContactPage() {
             <button>Email</button>
         </div>
     </div>
-     <div className='w-full flex items-center justify-center flex-col p-8 md:p-16 space-y-8 md:space-y-6' id='contact'>
-        <p>Contact</p>
-         <h6 className='text-4xl font-bond font-700'>Contact</h6>
-         <p className='text-sm font-light font-200'> Reach out and solve your engineering challenges today.</p>
-         <div className='block w-full  md:flex items-center justify-between space-y-4'>
-             <div className='space-y-4 w-full mx-auto '>
-                 
-                 <div className='space-y-2'>
-                     <FaRegEnvelopeOpen/>
-                     <p>General Inqueries:</p>
-                 </div>
-                 <div className='space-y-2'>
-                     <FaPhoneAlt/>
-                     <p>General Inqueries:</p>
-                 </div>
-                 <div className='space-y-2'>
-                     <FaAddressCard/>
-                     <p>General Inqueries:</p>
-                 </div>
-             </div>
-             <div className='w-full mx-auto '>
-                 {/* <form className='w-full  space-y-4  mx-auto'>
-                     <div className='border '>
-                         <input type="email" name="email" id="" placeholder='enter email here' className='p-4'/>
-                     </div>
-                     <div className='border '>
-                         <input type="text" name="name" id="" placeholder='enter email here' className='p-4'/>
-                     </div>
-                     <div className='border '>
-                         <input type="text" name="message" id="" placeholder='enter email here' className='p-4'/>
-                     </div>
-                     <div className='w-full mx-auto '>
-                         <button className='w-full p-2 border'>Submit</button>
-                     </div>
-                 </form> */}
-                 image
-             </div>
-         </div>
- 
- 
-     </div>
+     
         <div className='w-full flex items-center justify-center flex-col p-8 md:p-16 space-y-8 md:space-y-6' id='contact'>
         <p>Connect</p>
          <h6 className='text-4xl font-bond font-700'>Contact Us</h6>
