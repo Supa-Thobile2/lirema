b26e3032111d1efed4129d1ca695f86fca213258
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-24 20:44:26 +0200

    updated about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,11 +6,11 @@ import { Button } from '../components'
 function About() {
   return (
 
-    <div className='block  w-full p-4 space-y-4  md:flex' id='about'>
-    <div className=' w-full  space-y-2 md:w-1/2  flex flex-col space-y-2 md:space-y-8  md:p-16  mx-auto'>
+    <div className='block  w-full p-2 space-y-4  md:flex' id='about'>
+    <div className=' w-full  space-y-4 md:w-1/2  flex flex-col space-y-2 md:space-y-8  md:p-16  mx-auto'>
         <p className='text-[.8rem] font-semibold font-700 text-left'>About </p>
         <div className='space-y-2'>
-        <h1 className='text-lg md:text-xl font-bold'>Transforming engineering challenges into innovative solutions </h1>
+        <h1 className='text-[1.6rem]  font-bold font-600'>Transforming engineering challenges into innovative solutions </h1>
         <p className='text-sm font-light font-400'>LiRema Engineering and Construction delivers comprehensive technical services with unmatched precision. We combine technical expertise with strategic innovation to solve complex engineering challenges. </p>
         </div>
         <div className='flex items-centre justify-between '>
