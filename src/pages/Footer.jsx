6815615d6ab58bbb5c300bcbae882e3287456f92
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-25 00:09:27 +0200

    updated footer section

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
 
 
         <div className=' space-y-4 w-full'>
-          <h1 className='text-[1.5rem] font-bold font-700'>Li Rema Engineering & Construction</h1>
+          <h1 className='text-[1rem] font-bold font-700'>Li Rema Engineering & Construction</h1>
           <div className='space-y-2'>
             <ul className='block space-y-2 md:flex items-center justify-evenly '>
               <li className=' text-[.9rem] font-regular font-400'>Home</li>
@@ -21,7 +21,7 @@ function Footer() {
 
           <div className='block space-y-2 md:flex items-center justify-between'>
             <div>
-              <p className='text-[1rem] font-light font-700'> 2025 <strong>LiRema Construction</strong> All Rights Reserved.</p>
+              <p className='text-[.7rem] font-light font-700'> 2025 <strong>LiRema Construction</strong> All Rights Reserved.</p>
             </div>
             <div className='block md:flex items-center justify-between space-y-2'>
               <p  className='text-[1rem] font-light font-700'>Privacy Policy</p>
