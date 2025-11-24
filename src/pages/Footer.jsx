acb888d0df8e83e6095425a225f31fd93270c931
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-25 00:00:46 +0200

    updated footer section

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,30 +2,30 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-full p-4 '>
+    <div className='w-full p-8 '>
     
 
 
         <div className=' space-y-4 w-full'>
-          <h1>logo</h1>
-          <div >
+          <h1 className='text-[1.5rem] font-bold font-700'>Li Rema Engineering & Construction</h1>
+          <div className='space-y-2'>
             <ul className='block space-y-2 md:flex items-center justify-evenly '>
-              <li className='p-2 text-sm font-regular font-400'>Home</li>
-              <li className='p-2 text-sm font-regular font-400'>Services</li>
-              <li className='p-2 text-sm font-regular font-400'>About</li>
-              <li className='p-2 text-sm font-regular font-400'>Contact</li>
-              <li className='p-2 text-sm font-regular font-400'>Projects</li>
+              <li className='p-2 text-[.9rem] font-regular font-400'>Home</li>
+              <li className='p-2 text-[.9rem] font-regular font-400'>Services</li>
+              <li className='p-2 text-[.9rem] font-regular font-400'>About</li>
+              <li className='p-2 text-[.9rem] font-regular font-400'>Contact</li>
+              <li className='p-2 text-[.9rem] font-regular font-400'>Projects</li>
             </ul>
 
           </div>
 
           <div className='block space-y-2 md:flex items-center justify-between'>
             <div>
-              <p className='text-sm font-light'> 2025 <strong>LiRema Construction</strong> All Rights Reserved.</p>
+              <p className='text-[1rem] font-light font-700'> 2025 <strong>LiRema Construction</strong> All Rights Reserved.</p>
             </div>
             <div className='block md:flex items-center justify-between space-y-2'>
-              <p>Privacy Policy</p>
-              <p>Terms Of Service</p>
+              <p  className='text-[1rem] font-light font-700'>Privacy Policy</p>
+              <p  className='text-[1rem] font-light font-700'>Terms Of Service</p>
               {/* <p>Cookie setting</p> */}
             </div>
           </div>
