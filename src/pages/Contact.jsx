3ba577c86b5efa62c65b391458e4ddf596159aaa
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-24 23:38:08 +0200

    updated contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,22 +7,22 @@ import { Button } from '../components'
 function Contact() {
   return (
     <div className='w-full flex items-center justify-center flex-col p-8 md:p-16 space-y-8 md:space-y-6' id='contact'>
-        <h6 className='text-4xl font-bond font-700'>Contact</h6>
-        <p className='text-sm font-light font-200'> We're ready to discuss your engineering construction needs</p>
+        <h6 className='text-[1.5rem] font-bond font-700'>Contact</h6>
+        <p className='text-[.6rem] font-light font-200'> We're ready to discuss your engineering construction needs</p>
         <div className='block w-full  md:flex items-center justify-between space-y-4'>
             <div className='space-y-4 w-full mx-auto '>
                 
                 <div className='space-y-2'>
                     <FaRegEnvelopeOpen/>
-                    <p>General Inqueries:</p>
+                    <p className='text-[.6rem] font-light font-300'>General Inqueries:</p>
                 </div>
                 <div className='space-y-2'>
                     <FaPhoneAlt/>
-                    <p>General Inqueries:</p>
+                    <p className='text-[.6rem] font-light font-300'>General Inqueries:</p>
                 </div>
                 <div className='space-y-2'>
                     <FaAddressCard/>
-                    <p>General Inqueries:</p>
+                    <p className='text-[.6rem] font-light font-300'>General Inqueries:</p>
                 </div>
             </div>
             <div className='w-full mx-auto '>
