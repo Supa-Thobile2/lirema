6654699edf31530c64d8adba98504b00bb9a1ac2
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-24 19:58:30 +0200

    updated hero section typography

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -12,13 +12,16 @@ function Hero() {
   // }
   return (
 
-    <div className='block space-y-2 md:flex  md:p-18 mt-8 md:mt-14 ' id='home'>
+    <div className='block md:flex  md:p-18 mt-8 md:mt-14 ' id='home'>
     <div className='w-full mx-auto space-y-4 md:w-1/2  p-8 md:p-18 xl:p-20 md:space-y-16 lg:p-16 lg:space-y-18 text-center  flex items-center justify-center flex-col  mx-auto'>
        <div className='space-y-2'>
-        <h1 className='text-3xl
-         md:text-3xl font-bold font-300'>Engineering Solutions that build tommorrow</h1>
+        <div className='space-y-2'>
+        <h1 className='text-[1.4rem]
+         font-bold font-900'>Engineering Solutions that build tomorrow</h1>
         <p className=' text-[.9rem] text-tracking-wider md:text-center text-sm font- font-400'><strong>Li Rema construction </strong> 
         delivers comprehensive engineering and construction services with precision and expertise. We transform complex challenges into seamless technical solutions.</p>
+       
+       </div>
         </div> 
         
         <div className='w-full mx-auto flex items-center justify-center gap-8'>
