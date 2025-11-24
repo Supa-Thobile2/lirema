a44152558d2b8a5cc4e45681698f2c3b3716f364
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-11-24 21:15:03 +0200

    updated about section

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -7,7 +7,7 @@ function ServicesCard(props) {
   return (
     <div className=' border  '>
       <img src={img} alt='' className='object-fit block h-full'/>
-        <div className='space-y-4 p-2 md:p-4 '>
+        <div className='space-y-2 p-4'>
             <h6 className='text-[1rem] font-bold font-400'>{props.heading}</h6>
             <h1 className='text-[.7rem] font-semibold font-400'>{props.subtext}</h1>
             <p className='text-[.6rem] font-medium font-200'>{props.description}</p>
